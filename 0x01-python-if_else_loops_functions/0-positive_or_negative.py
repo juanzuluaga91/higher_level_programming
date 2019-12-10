adea21a40003c29c7cae6463c32a6a94119097bc
Juan Pablo <juanzuluaga91@gmail.com>
2019-12-10 18:57:29 -0500

    [Fix] Correcting loop statement

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-
 if number > 0:
     print("{:d}".format(number), 'is positive')
-if number == 0:
+elif number == 0:
     print("{:d}".format(number), 'is zero')
-if number < 0:
+else:
     print("{:d}".format(number), 'is nevative')
