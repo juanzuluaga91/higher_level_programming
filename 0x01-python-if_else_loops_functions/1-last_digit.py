b861111b3b51c41f9a50918ea94163152b40509c
Juan Pablo <juanzuluaga91@gmail.com>
2019-12-10 19:34:05 -0500

    [Fix] fixing nevative results

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+number = -4205
 ld = number % 10
+if number < 0:
+    ld *= -1
 if ld > 5:
     result = 'and is greater than 5'
 elif ld < 6 and ld != 0:
