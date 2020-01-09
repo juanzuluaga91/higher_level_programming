26ef820595469390c5f9062f4d0c31e07defd212
Juan Pablo <juanzuluaga91@gmail.com>
2020-01-08 19:18:21 -0500

    [Fix] Permissions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        quotient = a / b
+        x = a / b
     except ZeroDivisionError:
-        quotient = None
+        x = None
     finally:
-        print("Inside result: {}".format(quotient))
-        return quotient
+        print("Inside result: {}".format(x))
+        return x
