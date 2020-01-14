ec909ca6fba0d28a692044b8bc1f3ca012121bfc
Juan Pablo Zuluaga Pizza <juanzuluaga@MacBook-de-Juan.local>
2020-01-14 16:55:35 -0500

    [task 1]

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,45 @@
 #!/usr/bin/python3
-'''Module for a class named Rectangle.'''
+'''Module for Rectangle class.'''
 
 
 class Rectangle:
-    '''This class defines an empty Rectangle.'''
-    pass
+    '''
+    This class defines a simple Rectangle.
+    '''
+
+    def __init__(self, width=0, height=0):
+        '''
+        Constructor.
+        '''
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        '''Property for the width of the rectangle.
+        '''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        '''
+        Property for the height of the rectangle.
+        '''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+    
