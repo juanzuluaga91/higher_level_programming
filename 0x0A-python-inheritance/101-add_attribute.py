7ed86ccf21371f085ae9157d27e8b59caf392ca7
Juan Pablo Zuluaga Pizza <juanzuluaga@MacBook-de-Juan.local>
2020-01-20 22:09:51 -0500

    [Fix pycodestyle]

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ Function that add an attribute if possible """
+
+
 def add_attribute(obj, name, value):
     if hasattr(obj, '__dict__') is False:
         raise TypeError("can't add new attribute")
