fa6dc9da81a817f812f8b4cfcea870ce9ad41fd7
juanzuluaga91 <juanzuluaga91@gmail.com>
2020-03-02 17:10:54 -0500

    [101-102 task]

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,8 +1,7 @@
 -- Write a script that displays the average temperature (Fahrenheit) 
 -- by city ordered by temperature (descending).
 
-SELECT City, AVG(value) AS avg_temp
+SELECT city, AVG(value) AS avg_temp 
 FROM temperatures
-WHERE month = 7 OR month = 8
-GROUP BY City DESC
-ORDER By avg_temp DESC LIMIT 3;
+GROUP BY city 
+ORDER by avg_temp DESC;
---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+-- Write a script that displays the top 3 of cities temperature 
+-- during July and August ordered by temperature (descending)
+
+SELECT City, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month = 7 OR month = 8
+GROUP BY City DESC
+ORDER By avg_temp DESC LIMIT 3;
