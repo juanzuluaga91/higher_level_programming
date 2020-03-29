7fcc388ea1b8e6350c2ee9fad694206b1b0976c5
juanzuluaga91 <juanzuluaga91@gmail.com>
2020-03-29 13:21:40 -0500

    [state as argument]

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" prints the first State object from the database hbtn_0e_6_usa
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+
+if __name__ == "__main__":
+    if len(argv) == 5:
+        url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            argv[1], argv[2], argv[3], pool_pre_ping=True)
+        engine = create_engine(url)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        query = session.query(State).filter(State.name == argv[4]
+                                            .split('\'')[0]).order_by(State.id)
+        if len(query.all()) != 0:
+            for ins in query:
+                print("{}".format(ins.id))
+        else:
+            print("Not found")
