951504899fcf581c3e4404b8958b768105474fa2
juanzuluaga91 <juanzuluaga91@gmail.com>
2020-03-29 13:39:38 -0500

    pycodestyle fix

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,8 +1,10 @@
+  
 #!/usr/bin/python3
-""" 12-model_state_update_id_2 """
+""" 14-model_city_fetch_by_state """
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from model_city import City
 from sys import argv
 
 
@@ -13,6 +15,8 @@ if __name__ == "__main__":
         engine = create_engine(url)
         Session = sessionmaker(bind=engine)
         session = Session()
-        query = session.query(State).filter(State.id == 2)
-        query[0].name = 'New Mexico'
-        session.commit()
+        query = session.query(State, City).\
+            join(City, State.id == City.state_id).\
+            order_by(City.id)
+        for state, city in query:
+            print('{}: ({}) {}'.format(state.name, city.id, city.name))
