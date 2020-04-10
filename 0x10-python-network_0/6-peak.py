4d3daf4c1fa2b83ab1e9f4531a683e9e560a88dc
juanzuluaga91 <juanzuluaga91@gmail.com>
2020-04-10 07:10:47 -0500

    pycodestyle

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,7 +9,9 @@ def find_peak(list_of_integers):
     if l == 0:
         return
     x = l // 2
-    if (x == l - 1 or l_int[x] >= l_int[x + 1]) and (x == 0 or l_int[x] >= l_int[x - 1]):
+    if (x == l - 1 or
+        l_int[x] >= l_int[x + 1]) and\
+            (x == 0 or l_int[x] >= l_int[x - 1]):
         return l_int[x]
     if x != l - 1 and l_int[x + 1] > l_int[x]:
         return find_peak(l_int[x + 1:])
