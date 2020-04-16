a85e911f10d575de60b42a385ed3ec372b648fa8
juanzuluaga91 <juanzuluaga91@gmail.com>
2020-04-16 06:23:30 -0500

    5,6,7 task

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    response = requests.get(argv[1])
+    print(response.headers.get('X-Request-Id'))
