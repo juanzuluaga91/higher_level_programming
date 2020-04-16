35074e306186d40fb05f5ba7dfea5e4f1732d337
juanzuluaga91 <juanzuluaga91@gmail.com>
2020-04-16 06:26:07 -0500

    Fix

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,6 +5,5 @@
 if __name__ == '__main__':
     import requests
     from sys import argv
-    payload = {'email': argv[2]}
-    r = requests.post(argv[1], data=payload)
-    print(r.text)
+    response = requests.post(argv[1], data={'email': argv[2]})
+    print(response.text)
