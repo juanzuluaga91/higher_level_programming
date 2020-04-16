a3e730f848aa7a4f75dda6b1cbb37e23f81b2430
juanzuluaga91 <juanzuluaga91@gmail.com>
2020-04-16 06:15:16 -0500

    pep8

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+
+"""
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    req = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
