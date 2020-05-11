514021d8b89a5a678a37b05dd6228d26c5e02bf2
juanzuluaga91 <juanzuluaga91@gmail.com>
2020-05-11 18:45:19 -0500

    [7]

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+exports.nbOccurences = function (list, finder) {
+  let oc = 0;
+  for (let index = 0; index in list; index++) {
+    if (list[index] === finder) {
+      oc++;
+    }
+  }
+  return oc;
+};
