fee1d95668ca09baec8805ce67fd137b73c9e00e
juanzuluaga91 <juanzuluaga91@gmail.com>
2020-05-11 21:23:33 -0500

    [semistandard fix]

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-    let rev = [];
-    for (let i = 0; i < list.length; i++) {
-      rev.unshift(list[i]);
-    }
-    return rev;
-  };
-  
+  const rev = [];
+  for (let i = 0; i < list.length; i++) {
+    rev.unshift(list[i]);
+  }
+  return rev;
+};
